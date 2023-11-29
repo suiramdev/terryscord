c63b5c5d805b78867d04bda928722e5cc2199803
suiramdev <mariusnouchet@gmail.com>
2023-11-29 12:38:30 +0100

    :art: Moved logs in a specific folder

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,7 +1,7 @@
 import { Logger, ILogObj } from "tslog";
 import { createStream } from "rotating-file-stream";
 
-const stream = createStream("./data/tslog.log", {
+const stream = createStream("./logs/tslog.log", {
     size: "10M", // rotate every 10 MegaBytes written
     interval: "1d", // rotate daily
     compress: "gzip", // compress rotated files
